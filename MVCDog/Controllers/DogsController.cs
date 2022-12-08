82407271e56f2b8dbadeee299a71ed46d2ecd4d5
Neville Rabakale <neville.rabakale@gmail.com>
2022-12-08 14:18:01 +0100

    Updated project to Exercise 4

--- MVCDog/Controllers/DogsController.cs
+++ MVCDog/Controllers/DogsController.cs
@@ -1,19 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCDog.Models;
 
 namespace MVCDog.Controllers
-{
+{   
     public class DogsController : Controller
     {
+        DogService service;
         public DogsController()
         {
-
+            service= new DogService();
         }
 
         [HttpGet("")]
         [HttpGet("index")]
         public IActionResult Index()
         {
+            var model = service.GetAllDogs();
+            return View(model);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(Dog obj)
+        {
+            service.Add(obj);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            var model = service.GetDogById(id);
+            return View(model);
+        }
+
+        [HttpPost("edit/{id}")]
+        public IActionResult Edit(Dog obj)
+        {
+
+            service.EditDog(obj); 
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- MVCDog/Models/DogService.cs
+++ MVCDog/Models/DogService.cs
@@ -1,14 +1,18 @@
 ﻿namespace MVCDog.Models
 {
-    public class DataService
+    public class DogService
     {
         static List<Dog> Dogs = new List<Dog>()
         {
-            new Dog{Id = 1, Name = "Fluffy", Age = 4 } 
+            new Dog{Id = 0, Name = "Fluffy", Age = 4 } 
         };
 
         public void Add(Dog dog) 
-        { 
+        {
+            if(Dogs.Count != 0)
+            {  
+                 dog.Id = Dogs.Max(o => o.Id); 
+            }
             Dogs.Add(dog); 
         }
         public void Remove(Dog dog)
@@ -16,6 +20,12 @@
             Dogs.Remove(dog);
         }
 
+        public void EditDog(Dog dog) 
+        {
+            GetDogById(dog.Id);
+            dog.Name = dog.Name;
+            dog.Age= dog.Age;
+        }
 
         public Dog[] GetAllDogs()
         {
